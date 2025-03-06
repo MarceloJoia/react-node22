023ba032dadc62d53993bdc5a98282b424d28a09
Marcelo Joia <sitejoia@hotmai.com>
2025-03-06 16:32:08 -0300

    CRUD - Editar registro no banco de dados

--- src/controllers/SituationController.ts
+++ src/controllers/SituationController.ts
@@ -79,11 +79,6 @@ router.get("/situacoes/:id", async (req: Request, res: Response) => {
 });
 
 
-
-
-
-
-
 // CADASTRAR (rota) - Criar a rota para cadastrar a Situação
 // Endereço para acessar a api através da aplicação externa com o verbo POST: http://localhost:8080/situacoes
 // A aplicação externa deve indicar que está enviado os dados em formato de objeto: Content-Type: application/json
@@ -123,8 +118,80 @@ router.post("/situacoes", async (req: Request, res: Response) => {
         res.status(500).json({
             message: "Erro! Situação não pode ser cadastrada.",
         });
+        // Mata o processamento
+        return;
     }
 });
+
+
+// CADASTRAR (criar a rota) - Criar a rota para editar uma situação
+// Endereço para acessar a API através da aplicação externa com o verbo PUT: http://localhost:8080/situacoes/:id
+// A aplicação externa deve indicar que está enviado os dados em formato de objeto: Content-Type: application/json
+// Dados em formato de objeto
+/*
+{
+    "nameSituation": "Ativo"
+}
+*/
+router.put("/situacoes/:id", async (req: Request, res: Response) => {
+    // res.send("Editar");
+    try {
+
+        // Obter o ID da situação a partir dos parâmetros da requisição
+        const { id } = req.params; // Desestruturação
+
+        // Receber os dados enviados no corpo da requisição
+        const data = req.body;
+
+        // Obter o repositório da entidade Situation
+        const situationRepository = AppDataSource.getRepository(Situation);
+
+        // Buscar a situação no banco de dados pelo ID
+        const situation = await situationRepository.findOneBy({ id: parseInt(id) });
+
+        // Verificar se a situação foi encontrada
+        if (!situation) {
+            res.status(404).json({
+                message: "Atensão! Situação não encontrada!"
+            });
+            // Mata o processamento
+            return;
+        }
+
+        // Atualizar os dados da situação
+        situationRepository.merge(situation, data);
+
+        // Salvar as alterações no banco de dados
+        const updateSituation = await situationRepository.save(situation);
+
+        // Retornar resposta de sucesso
+        res.status(201).json({
+            message: "Sucesso! Situação Editada.",
+            situation: updateSituation, // Recebe o retorno do banco
+        });
+
+        // Mata o processamento
+        return;
+
+    } catch (error) {
+        // Retornar resposta de Erro
+        res.status(500).json({
+            message: "Erro! Situação não pode ser Editada.",
+        });
+        // Mata o processamento
+        return;
+    }
+});
+
+
+
+
+
+
+
+
+
+
 
 // Exportar a instrução que está dentro da constante router
 export default router;
